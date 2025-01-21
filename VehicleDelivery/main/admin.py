13db3f4ed5fbdbe73b303273194fcd14edc7ab90
Vi-Mi <vierka.m.blazickova@gmail.com>
2025-01-21 18:35:22 +0100

    added roles to database for users

--- VehicleDelivery/main/admin.py
+++ VehicleDelivery/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department, Person, ClaimModel
 
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Person)
+admin.site.register(ClaimModel)
--- VehicleDelivery/main/models.py
+++ VehicleDelivery/main/models.py
@@ -2,21 +2,33 @@ from django.db import models
 from django.utils import timezone
 from datetime import date
 import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Person(models.Model):
+
+
+class Department(models.Model):
+    # code = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    profile_image = models.ImageField(upload_to='profile_images/')
-    CATEGORY_CHOICES = [
-        ('ST', 'Student'),
-        ('TE', 'Teacher'),
-        ('OT', 'Other'),
-    ]
-    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='ST')
+
 
     def __str__(self):
         return self.name
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    TYPE_OF_USER = [
+        ('ADMIN', 'Admin'),
+        ('AGENT', 'Agent'),
+    ]
+    # category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    userType = models.CharField(max_length=5, choices=TYPE_OF_USER, default='AGENT')
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
 
 
 
